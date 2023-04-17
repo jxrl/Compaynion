5bce9e2e554e2c8ac129c2242a890949233070a6
Giselle <abc>
2023-04-17 18:49:57 +0530

    Gis- DB connectivity of login pages

--- app/src/main/java/com/example/compaynion/AdminLogin.java
+++ app/src/main/java/com/example/compaynion/AdminLogin.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdminLogin extends AppCompatActivity {
 
+    private EditText username, password;
     private Button logb4;
     private Button startb4;
+    private DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
 
+        username= (EditText) findViewById(R.id.adminUsername);
+        password= (EditText) findViewById(R.id.adminPassword);
         logb4 = (Button) findViewById(R.id.adminloginButton);
         startb4 = (Button) findViewById(R.id.adminMainPage);
-
+        db= new DBHelper(this);
 
         //intent to go back to start page
         startb4.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +41,24 @@ public class AdminLogin extends AppCompatActivity {
         logb4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(AdminLogin.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass=db.checkusernamepassword(user,pass);
+                    if(checkuserpass==true)
+                    {
+                        Toast.makeText(AdminLogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(AdminLogin.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
                 Intent i2 = new Intent(AdminLogin.this,AdminHomepage.class);
                 startActivity(i2);
             }
--- app/src/main/java/com/example/compaynion/DBHelper.java
+++ app/src/main/java/com/example/compaynion/DBHelper.java
@@ -1,35 +1,40 @@
 package com.example.compaynion;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHandler
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper
 {
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-    public class DBHelper extends SQLiteOpenHelper
-    {
-        static String DATABASE_NAME="Compaynion";
-        public static final String TABLE_NAME="student";
-        public static final String Table_Column_ID="id";
-        public static final String Table_Column_RollNo="rollno";
-        public static final String Table_Column_Name="name";
-        public static final String Table_Column_Class="class";
-        public DBHelper(Context context)
-        {
-            super(context, DATABASE_NAME, null, 1);
-        }
-        @Override
-        public void onCreate(SQLiteDatabase database)
-        {
-            String CREATE_TABLE="CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ("+Table_Column_ID+" INTEGER PRIMARY KEY, "+Table_Column_RollNo+" VARCHAR,  "+Table_Column_Name+" VARCHAR, "+Table_Column_Class+" VARCHAR)";  database.execSQL(CREATE_TABLE);
-        }
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)  {
-            db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
-            onCreate(db);
-        }
+    public static final String DBNAME="Compaynion.db";
+
+    public DBHelper( Context context) {
+        super(context, "Compaynion.db" , null, 1);
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS users");
+    }
+
+    public Boolean checkusernamepassword (String username, String password)
+    {
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from users where username= ? and password= ?", new String[] {username,password});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
 
 }
